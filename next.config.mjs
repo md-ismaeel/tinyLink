1994fbba94aa4b6c9d01ae55e6fd05a64441b6ea
Md Ismail <mdismaeelkhan345@gmail.com>
2025-11-21 02:32:20 +0530

    fixed redirection issues

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,16 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async redirects() {
+    return [
+      {
+        // This will redirect /old-route/123 to /new-route/123
+        source: '/old-route/:code',
+        destination: '/new-route/:code',
+        permanent: true, // Use 301 for permanent move (good for SEO)
+      },
+    ]
+  },
 }
 
 export default nextConfig
